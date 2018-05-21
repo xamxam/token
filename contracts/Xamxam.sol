0404faed28e35b483fce5e35d49c39b57e0cc995
diop <fodesdiop@gmail.com>
2018-05-20 21:15:21 -0700

    Polish contract

--- contracts/Xamxam.sol
+++ contracts/Xamxam.sol
@@ -1,78 +1,133 @@
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.23;
 
 import "zeppelin-solidity/contracts/math/SafeMath.sol";
-import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 import "zeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract Xamxam is Ownable, ERC20 {
-  using SafeMath for uint256;
+    using SafeMath for uint256;
 
-  string public constant name = "Xamxam";
-  string public constant symbol = "XAM";
-  uint8 public constant decimals = 18;
+    string public constant name = "Xamxam";
+    string public constant symbol = "XAM";
+    uint8 public constant decimals = 18;
 
-  mapping(address => uint256) balances;
+    mapping(address => uint256) balances;
 
-  mapping(address => mapping (address => uint256)) allowed;
+    mapping(address => mapping (address => uint256)) allowed;
 
-  uint internal totalSupply_;
-  uint public INITIAL_SUPPLY;
+    uint internal totalSupply_;
+    uint public INITIAL_SUPPLY;
 
-  address public owner;
+    address public owner;
 
-  event Transfer(address indexed from, address indexed to, uint tokens);
-  event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
-  event OwnershipTransferred(address indexed from, address indexed to);
+    struct Student {
+        uint256 stipend;
+        uint256 lockValue;
+        uint256 lockEndTime;
 
-  function Xamxam() {
-    INITIAL_SUPPLY = 100000000000000000;
-    balances[msg.sender] = INITIAL_SUPPLY;
-    totalSupply_ = INITIAL_SUPPLY;
+        mapping(address => uint256) authorized;
+    }
 
-    owner = msg.sender;
-    emit Transfer(address(0), owner, totalSupply_);
-  }
+    mapping(address => Student) public students;
+    mapping(address => uint256) public stipendOf;
+    mapping(address => uint256) public freezeOf;
+    mapping(address => uint256) public balanceOf;
 
-  function totalSupply() public view returns (bool success) {
-    return totalSupply_;
-  }
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+    event Freeze(address indexed from, uint256 tokens);
+    event Unfreeze(address indexed from, uint tokens);
+    event Burn(address indexed from, uint256 tokens);
+    event OwnershipTransferred(address indexed from, address indexed to);
 
-  function balanceOf(address tokenOwner) public view returns (uint256 balance) {
-    return balances[tokenOwner];
-  }
+    function constructor() public {
+        INITIAL_SUPPLY = 100000000000000000;
+        balances[msg.sender] = INITIAL_SUPPLY;
+        totalSupply_ = INITIAL_SUPPLY;
 
-  function allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
-    return allowed[tokenOwner][spender];
-  }
+        owner = msg.sender;
+        emit Transfer(address(0), owner, totalSupply_);
+    }
 
-  function approve(address spender, uint tokens) public returns (bool success) {
-    allowed[msg.sender][spender] = tokens;
-    emit Approval(msg.sender, spender, tokens);
-    return true;
-  }
+    function totalSupply() public view returns (bool success) {
+        return totalSupply_;
+    }
 
-  function transfer(address from, uint tokens) public returns (bool success) {
-    balances[msg.sender] = balances[msg.sender].sub(tokens);
-    balances[to] = balances[to].add(tokens);
-    emit Transfer(msg.sender, to, tokens);
-    return true;
-  }
+    function balanceOf(address tokenOwner) public view returns (uint256 balance) {
+        return balances[tokenOwner];
+    }
 
-  function transferFrom(address from, address to, uint tokens) public returns (bool success) {
-    balances[from] = balances[from].sub(tokens);
-    allowed[from][msg.sender] = allowed[from][msg.sender].sub(tokens);
-    balances[to] = balances[to].add(tokens);
-    emit Transfer(from, to, tokens);
-    return true;
-  }
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
+        return allowed[tokenOwner][spender];
+    }
 
-  function setOwner(address newOwner) public onlyOwner {
-    owner = newOwner;
-    emit OwnershipTransferred(owner, newOwner);
-  }
+    function approve(address spender, uint tokens) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        success = true;
+    }
 
-  function () public payable {
-    revert();
-  }
+    function transfer(address to, uint tokens) public returns (bool success) {
+        if (to == 0x0) revert();
+        if (tokens <= 0) revert();
+
+        balances[msg.sender] = balances[msg.sender].sub(tokens);
+        balances[to] = balances[to].add(tokens);
+        emit Transfer(msg.sender, to, tokens);
+        success = true;
+    }
+
+    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+        if (to == 0x0) revert();
+        if (tokens <= 0) revert();
+        if (balanceOf[from] < tokens) revert();
+        if (balanceOf[to] + tokens < balanceOf[to]) revert();
+        if (tokens > allowance[from[msg.sender]]) revert();
+        
+        balances[from] = balances[from].sub(tokens);
+        allowed[from][msg.sender] = allowed[from][msg.sender].sub(tokens);
+        balances[to] = balances[to].add(tokens);
+        emit Transfer(from, to, tokens);
+        success = true;
+    }
+
+    function freeze(uint256 tokens) public returns (bool success) {
+        if(balanceOf[msg.sender] < tokens) revert();
+        if(tokens <= 0) revert();
+
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(tokens);
+        freezeOf[msg.sender] = freezeOf[msg.sender].add(tokens);
+        emit Freeze(msg.sender, tokens);
+        success = true;
+    }
+
+    function unfreeze(uint256 tokens) public returns (bool success) {
+        if (freezeOf[msg.sender] < tokens) revert();
+        if (tokens <= 0) revert();
+
+        freezeof[msg.sender] = freezeOf[msg.sender].sub(tokens);
+        balanceOf[msg.sender] = balanceOf[msg.sender].add(tokens);
+        emit Unfreeze(msg.sender, tokens);
+        success == true;
+    }
+
+    function burn(uint256 tokens) public returns (bool success) {
+        if (balanceOf[msg.sender] < tokens) revert();
+        if (tokens <= 0) revert();
+
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(tokens);
+        totalSupply = totalSupply.sub(tokens);
+        emit Burn(msg.sender, tokens);
+
+        success = true;
+    }
+
+    function withdrawEther(uint256 tokens) public {
+        if(msg.sender != owner) revert();
+        owner.transfer[tokens];
+    }
+
+    function () public payable {
+    }
 
 }
